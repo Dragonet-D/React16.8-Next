5687d3d46f73799f6e2bdb212d2b4b5c0d52213d
Deng XiaoLong <Dragonet_d@163.com>
2019-07-28 12:19:48 +0800

    koa router

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 const koa = require('koa');
 const next = require('next');
+const Router = require('koa-router');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 // app.prepare().then(() => {
-  const server = new koa();
-  server.use(async (ctx, next) => {
-    const {path, method} = ctx;
-    // next 调用下个中间件
-    ctx.body = `<span>Koa Render ${method} ${path}</span>`;
-    // await next()
-  });
+const server = new koa();
+const router = new Router();
 
-  server.use(async (ctx, next) => {
-    ctx.body = '<span>Koa Render2</span>'
-  });
-  // server.use(async (ctx, next) => {
-  //   await handle(ctx.req, ctx.res);
-  //   ctx.respond = false;
-  // });
-  server.listen(3000, () => {
-    console.log('koa server listening on 3000');
-  });
+router.get('/test/:id', (ctx) => {
+  ctx.body = `<p>request / text ${ctx.params.id}</p>`
+});
+
+server.use(async (ctx, next) => {
+  // const {path, method} = ctx;
+  // next 调用下个中间件
+  // ctx.body = `<span>Koa Render ${method} ${path}</span>`;
+  await next()
+});
+
+server.use(router.routes());
+
+server.use(async (ctx, next) => {
+  ctx.body = '<span>Koa Render2</span>'
+});
+// server.use(async (ctx, next) => {
+//   await handle(ctx.req, ctx.res);
+//   ctx.respond = false;
+// });
+server.listen(3000, () => {
+  console.log('koa server listening on 3000');
+});
 // });
